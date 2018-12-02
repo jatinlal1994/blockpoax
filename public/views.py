84f7d15bb4e71362d9405b1ed3a57a4782136346
Jatin Lal <jatinlal1994@gmail.com>
2018-12-02 23:35:57 +0530

    Pushing code

--- public/views.py
+++ public/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, 'public/pages/home.html', {})
+
+def aboutUs(request):
+	return render(request, 'public/pages/about-us.html', {})
+
+def privacyPolicy(request):
+	return render(request, 'public/pages/privacy-policy.html', {})
+
+def termsOfService(request):
+	return render(request, 'public/pages/terms-of-service.html', {})
+
+def faqs(request):
+	return render(request, 'public/pages/faqs.html', {})
+
+def contactUs(request):
+	return render(request, 'public/pages/contact-us.html', {})
--- socserv/urls.py
+++ socserv/urls.py
@@ -1,18 +1,3 @@
-"""socserv URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
@@ -20,5 +5,10 @@ from public import views as public
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', public.home, name='public'),
+    path('', public.home, name='home'),
+    path('about-us', public.aboutUs, name='about-us'),
+    path('privacy-policy', public.privacyPolicy, name='privacy-policy'),
+    path('terms-of-service', public.termsOfService, name='terms-of-service'),
+    path('contact-us', public.contactUs, name='contact-us'),
+    path('faqs', public.faqs, name='faqs'),
 ]
